59e25151f15aba2ab65e0fd9c4b180927bbff51c
zukhrofy abbar <abbarzukhrofy@gmail.com>
2026-09-24 02:43:50 +0700

    feat: simplify contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,12 +1,10 @@
 import Intro from "../components/contact/Intro";
-import Paths from "../components/contact/Paths";
 import ContactForm from "../components/contact/ContactForm";
 
 export default function Contact() {
   return (
     <main>
       <Intro />
-      <Paths />
       <ContactForm />
     </main>
   );
