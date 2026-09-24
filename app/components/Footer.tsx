51e7a20e8b30e6aca761fe61219d4a9ac59eddef
zukhrofy abbar <abbarzukhrofy@gmail.com>
2026-09-24 12:43:45 +0700

    fix: rework footer certification badges layout

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="bg-surface py-16 md:py-20 lg:py-25">
       <div className="container mx-auto flex flex-col gap-16 px-4">
         {/* footer header content */}
-        <div className="flex flex-col gap-10 md:flex-row md:items-start md:justify-between">
+        <div className="flex flex-col gap-10 md:flex-row md:items-center md:justify-between">
           <div className="flex flex-col gap-6">
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
@@ -11,26 +11,27 @@ export default function Footer() {
               alt="KSC Consultants"
               className="h-8.5 w-auto self-start"
             />
-            <p className="text-sm leading-relaxed text-muted-dark md:max-w-xs">
-              A recruitment consultancy built around a simple idea: better
-              recruitment starts with understanding people.
-            </p>
+
+            <div className="flex items-center gap-4">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src="/businessgood.png"
+                alt="Business For Good, Social Enterprise Member of raiSE Singapore"
+                className="h-20 w-auto shrink-0"
+              />
+              <p className="text-sm leading-relaxed text-muted-dark md:max-w-xs">
+                We are a Singapore social enterprise, a member of raiSE SG.
+              </p>
+            </div>
+
             <p className="text-xs font-medium tracking-[2.16px] text-primary uppercase">
               Est. 2011
             </p>
           </div>
 
           {/* certifications */}
-          <div className="flex items-center gap-6">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img src="/bizsafe.png" alt="bizSAFE 3" className="h-24 w-auto" />
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              src="/businessgood.png"
-              alt="Business For Good, Social Enterprise Member of raiSE Singapore"
-              className="h-24 w-auto"
-            />
-          </div>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src="/bizsafe.png" alt="bizSAFE 3" className="h-32 w-auto" />
         </div>
 
         {/* footer bottom content */}
